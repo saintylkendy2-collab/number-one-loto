9d153621c7df038da404fdb9cb9381e9bc72feeb
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-04-10 15:05:28 -0400

    fix error

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-app.get('/play', (req, res) => {
-const users = loadUsers();
-const user = req.cookies.user;
+const express = require("express");
+const app = express();
 
-if (!user || !users[user]) {
-return res.redirect('/');
-}
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
 res.send(`
 <html>
 <head>
@@ -13,68 +11,48 @@ res.send(`
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
 body {
-font-family: Arial;
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
-margin: 0;
-background: #f5f5f5;
-}
-
-.box {
-width: 95%;
-max-width: 500px;
-text-align: center;
-background: white;
+font-size: 22px;
 padding: 20px;
-border-radius: 10px;
-box-shadow: 0 0 10px rgba(0,0,0,0.1);
+text-align: center;
 }
 
-h3, h4 {
-margin: 5px 0;
-}
-
-textarea {
+input, button, textarea {
 width: 100%;
-height: 180px;
-font-size: 18px;
-padding: 10px;
-margin-top: 10px;
-}
-
-button {
-width: 100%;
-padding: 15px;
+padding: 16px;
 font-size: 20px;
 margin-top: 10px;
-border: none;
-border-radius: 5px;
-background: black;
-color: white;
 }
 </style>
 </head>
-
 <body>
-<div class="box">
-<h3>VANDÈ: ${users[user].nom}</h3>
-<h4>ID: ${user}</h4>
+<h2>NUMBER ONE LOTO</h2>
 
 <form method="POST" action="/print">
-<textarea name="data" placeholder="Egzanp:
+<textarea name="data" rows="10" placeholder="Egzanp:
 BOR 58 250
 BOR 85 100
 MAR 25*36 15"></textarea>
 <button type="submit">GENERATE</button>
 </form>
-
-<form method="POST" action="/logout">
-<button type="submit">LOGOUT</button>
-</form>
-</div>
 </body>
 </html>
 `);
+});
+
+app.post("/print", (req, res) => {
+const data = req.body.data;
+
+res.send(`
+<html>
+<body style="font-size:24px;">
+<pre>${data}</pre>
+<br>
+<a href="/">BACK</a>
+</body>
+</html>
+`);
+});
+
+app.listen(3000, () => {
+console.log("Server ap mache sou http://localhost:3000");
 });

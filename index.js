8140c3dd2933056104d7c70923d4120083525161
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-04-15 15:17:53 -0400

    loterie fixed

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/", (req, res) => {
+res.redirect("/dashboard");
+});
 app.get("/dashboard", (req, res) => {
 res.send(`
 <!DOCTYPE html>

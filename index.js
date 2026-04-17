98f7a5afbea853438deec97bb1dac612e68d0cd1
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-04-17 19:46:17 -0400

    loterie fixed

--- index.js
+++ index.js
@@ -1570,10 +1570,9 @@ gameLines.push(`${typeTxt}${numTxt}${amtTxt}`);
 <meta charset="UTF-8">
 <title>Print</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
 <style>
-
-
-.line{<style>
 html, body{
 margin:0;
 padding:0;

c8555b87ba7fa330043cb241792ce4af14ebed6d
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-04-24 18:52:12 -0400

    fix vendor rapports with calendar and print

--- index.js
+++ index.js
@@ -1584,21 +1584,6 @@ function clearLoteries(){
  updateFields();
 }
 
-function validateLoteries(){
- document.getElementById("loterieModal").classList.remove("show");
- document.getElementById("overlay").classList.remove("show");
-
- if(selectedLoteries.length === 0){
-   activeField = "loterie";
-   updateFields();
-   openLoterieModal();
-   return;
- }
-
- activeField = "montant";
- cursorMontant = montant.length;
- updateFields();
-}
 
 function toggleLoterie(name){
  var idx = selectedLoteries.indexOf(name);

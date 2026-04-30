59df090d05be5eda3885d9d7822087885ba87226
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-04-29 22:05:16 -0400

    update

--- admin.js
+++ admin.js
@@ -1519,16 +1519,17 @@ tbody tr:nth-child(even){background:#313652;}
   </div>
 </div>
 
-   <div style="padding:14px;">
-  <div id="ticketsFilters"></div>
-</div>
+ <div style="padding:14px;">
+      <div id="ticketsFilters"></div>
 
-<div class="table-scroll">
-  <table>
-    <thead id="ticketsHead"></thead>
-    <tbody id="ticketsBody"></tbody>
-  </table>
-</div>
+      <div class="table-scroll">
+        <table>
+          <thead id="ticketsHead"></thead>
+          <tbody id="ticketsBody"></tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 
 <div id="transactionsPage" class="page-block hidden">
   <div class="page-title">Transactions</div>

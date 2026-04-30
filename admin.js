a7fe6a5017c14ea2ce5262eb6b171dc07915b692
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-04-30 01:15:04 -0400

    update

--- admin.js
+++ admin.js
@@ -3729,7 +3729,9 @@ function openTicketDetail(ticketId){
       '<div style="margin-top:10px;"><b>Jugada:</b>' + (jeuxHTML || "Aucune") + '</div>' +
       '<div style="margin-top:10px;"><b>Total:</b> ' + formatAmount(ticket.total) + '</div>' +
       '<div><b>Premio:</b> ' + formatAmount(ticket.premio) + '</div>' +
-    '</div>';
+'<button onclick="window.opener.cancelTicket(\'' + safe(ticket.id) + '\'); window.close();" style="margin-top:16px;width:100%;height:46px;background:#ff5555;color:white;border:0;border-radius:10px;font-size:17px;font-weight:700;">ANILE TICKET</button>' +
+'<button onclick="window.close()" style="margin-top:10px;width:100%;height:42px;background:#444b70;color:white;border:0;border-radius:10px;font-size:16px;">TOUNEN</button>' +
+'</div>';
 
   var w = window.open("", "_blank");
   w.document.write(html);

3c2c110a4e04939aa3b780f7f6ffdb1b185b223a
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-01 20:53:14 -0400

    switch vendors to mongoose

--- models/vendor.js
+++ models/vendor.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const VendorSchema = new mongoose.Schema({
-  id: String,
-  clave: String,
-  nombre: String,
-  telefono: String,
-  direccion: String,
-  estatus: String
+const VendorSchema = new mongoose.Schema({}, {
+  strict: false,
+  timestamps: true
 });
+
+VendorSchema.index({ id: 1 }, { unique: true, sparse: true });
 
 module.exports = mongoose.model("Vendor", VendorSchema);

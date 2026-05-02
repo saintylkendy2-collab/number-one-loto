edef94065e2a8bb88dd0902bf8ccfd44565338ce
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-02 16:13:04 -0400

    switch vendors to mongoose

--- models/Sorteo.js
+++ models/Sorteo.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const SorteoSchema = new mongoose.Schema({
   date: { type: String, required: true },
   loteria: { type: String, required: true },
+
   r1: String,
-  r2: String
+  r2: String,
+  r3: String,
+  r4: String
 }, {
   timestamps: true
 });
 
-// Evite doublon (menm tirage 2 fwa)
 SorteoSchema.index({ date: 1, loteria: 1 }, { unique: true });
 
 module.exports = mongoose.model("Sorteo", SorteoSchema);

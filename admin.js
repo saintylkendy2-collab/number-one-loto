3fc93ffbdd6dfa8771f506f7e50d5e18b35d5494
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-01 20:26:08 -0400

    switch vendors to mongoose

--- admin.js
+++ admin.js
@@ -924,14 +924,12 @@ router.post("/master/ticket/:id/anile", async (req, res) => {
     </h2>
 
    <button
-onclick="
-  if (window.opener && !window.opener.closed) {
-    window.opener.location.href = '/master/vendors#tickets&reload=' + Date.now();
-    window.close();
-  } else {
-    history.back();
-  }
-"
+      onclick="
+        if (window.opener) {
+          window.opener.location.href = '/master/vendors#tickets&reload=' + Date.now();
+        }
+        window.close();
+      "
   style="margin-top:50px;height:65px;width:95%;font-size:22px;border-radius:10px;">
   RETOUNEN
 </button>

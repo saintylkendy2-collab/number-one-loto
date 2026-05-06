1992b3b3c4dc7c335cea2b1dfacac50a8dc2bd66
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-06 16:23:10 -0400

    update

--- admin.js
+++ admin.js
@@ -2974,9 +2974,9 @@ function renderTicketsReport(){
       '<td>' + safe(t.vendeurNom || t.vendeur) + '</td>' +
       '<td>' + (Array.isArray(t.jeux) ? t.jeux.length : 0) + '</td>' +
       '<td>' + formatAmount(t.total) + '</td>' +
-      '<td>' + formatAmount(t.premio) + '</td>' +
+      '<td>' + (t.premioLabel || formatAmount(t.premio || 0)) + '</td>' +
       '<td style="text-align:center;">' + getStatusIcon(t.status || "ANATAN") + '</td>' +
-     '<td><a class="mini-btn" href="/master/ticket/' + encodeURIComponent(t.id) + '" target="_blank">🔍</a></td>' +
+     '<td><button class="mini-btn" onclick="alert(\'ID: \' + \'' + safe(t.id) + '\')">🔍</button></td>' +
     '</tr>';
   }).join("");
 }

d47f8c858aaaeee56334d3b54c2a18f8e707ebc2
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-08 12:08:19 -0400

    update

--- models/Grupo.js
+++ models/Grupo.js
@@ -1,7 +1,9 @@
+const mongoose = require("mongoose");
+
 const GrupoSchema = new mongoose.Schema({
   nombre: { type: String, required: true, unique: true },
   estatus: { type: String, default: "Activo" },
   comisionGrupo: { type: Number, default: 0 }
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model("Grupo", GrupoSchema);

a6b5febbc045ef144c8b5f704ad0cfaa1960362d
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-12 02:11:58 -0400

    update

--- admin.js
+++ admin.js
@@ -3971,11 +3971,7 @@ function renderLoteriasAdmin(){
 
       '<td>' + safe(l.openTime || "") + '</td>' +
 
-      '<td>' + safe(
-  (l.closeDays && l.closeDays.monday)
-    ? l.closeDays.monday
-    : (l.closeTime || "")
-) + '</td>' +
+   '<td>' + safe(l.closeTime || "") + '</td>' +
 
       '<td>' + (l.limite ? 'YES' : '') + '</td>' +
 

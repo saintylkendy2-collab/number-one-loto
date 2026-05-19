ea550a0cee87cb0e7efcc4ee4921db86bb4caa69
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-18 21:32:29 -0400

    update

--- index.js
+++ index.js
@@ -755,19 +755,6 @@ if (credit <= 0) {
   });
 }
 
-var credito = Number(vendor.config && vendor.config.credito || 0);
-var balance = Number(vendor.balance || 0);
-var totalTicket = Number(total || 0);
-
-if(
-  credito > 0 &&
-  (balance + totalTicket) > credito
-){
-  return res.status(403).json({
-    ok:false,
-    message:"OU PA GEN KREDI"
-  });
-}
 
     const limites = limitesAjustes || {};
 
@@ -1622,6 +1609,20 @@ const finalJeux = jeux
     !(j.gratis === true || j.free === true)
   )
   .concat(freeMariages);
+
+  var credito = Number(vendor.config && vendor.config.credito || 0);
+var balance = Number(vendor.balance || 0);
+var totalTicket = Number(total || 0);
+
+if(
+  credito > 0 &&
+  (balance + totalTicket) > credito
+){
+  return res.status(403).json({
+    ok:false,
+    message:"OU PA GEN KREDI"
+  });
+}
 
     const ticket = await Ticket.create({
       id: ticketId,

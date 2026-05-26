5a79a6f13af78f0dbd2e5733dfbabcd935144a20
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-25 20:19:39 -0400

    update

--- index.js
+++ index.js
@@ -6066,48 +6066,64 @@ app.get("/print", async (req, res) => {
       return left + mid + right;
     }
 
-  const lotMap = {};
+    let lotSeen = {};
+    let loteriesText = "";
 
-(ticket.jeux || []).forEach(function(j){
-  let loterie = String(j.loterie || j.loteria || "").trim().toUpperCase() || "SANS TIRAGE";
+    (ticket.jeux || []).forEach(function(j){
+      const lot = String(j.loterie || j.loteria || "").trim().toUpperCase() || "SANS TIRAGE";
 
-  if (!lotMap[loterie]) lotMap[loterie] = [];
+      if (!lotSeen[lot]) {
+        lotSeen[lot] = true;
+        loteriesText += clean(lot) + NL;
+      }
+    });
 
-  let typeRaw = String(j.type || "").toUpperCase();
-  let numero = String(j.numero || "").trim();
-  let montant = Number(j.montant || 0);
+    const gameMap = {};
 
-  let type = typeRaw;
-  if (typeRaw === "BOR") type = "Borlette";
-  else if (typeRaw === "MAR") type = "Mariage";
-  else if (typeRaw === "L41") type = "Loto4";
+    (ticket.jeux || []).forEach(function(j){
+      if (j.gratis === true || j.free === true) {
+        return;
+      }
 
-  lotMap[loterie].push({
-    type: type,
-    numero: numero,
-    montant: montant,
-    gratis: j.gratis === true || j.free === true
-  });
-});
+      let typeRaw = String(j.type || "").toUpperCase();
+      let numero = String(j.numero || "").trim();
+      let montant = Number(j.montant || 0);
+      let loterie = String(j.loterie || j.loteria || "").trim().toUpperCase();
 
-let gamesText = "";
+      let type = typeRaw;
+      if (typeRaw === "BOR") type = "Borlette";
+      else if (typeRaw === "MAR") type = "Mariage";
 
-Object.keys(lotMap).forEach(function(loterie, index){
-  if (index > 0) {
-    gamesText += "------------------------------" + NL;
-  }
+      let key = loterie + "|" + type + "|" + numero + "|" + montant;
 
-  gamesText += clean(loterie) + NL;
-  gamesText += "------------------------------" + NL;
+      if (!gameMap[key]) {
+        gameMap[key] = {
+          loterie: loterie,
+          type: type,
+          numero: numero,
+          montant: montant,
+          count: 0
+        };
+      }
 
-  lotMap[loterie].forEach(function(g){
-    gamesText += lineGame(
-      g.type,
-      g.numero,
-      g.gratis ? "Gratis" : money(g.montant)
-    ) + NL;
-  });
-});
+      gameMap[key].count++;
+    });
+
+    const freeGames = (ticket.jeux || []).filter(function(j){
+      return j.gratis === true || j.free === true;
+    });
+
+    let freeMap = {};
+
+    freeGames.forEach(function(j){
+      let loterie = String(j.loterie || j.loteria || "").trim().toUpperCase() || "SANS TIRAGE";
+
+      if (!freeMap[loterie]) {
+        freeMap[loterie] = [];
+      }
+
+      freeMap[loterie].push(j);
+    });
 
     let text = "";
 
@@ -6115,6 +6131,9 @@ Object.keys(lotMap).forEach(function(loterie, index){
     text += "SELLER " + clean(sellerName) + NL;
     text += "TICKET " + clean(ticket.id || ticket.ticketId || ticket.serial || ticketId) + NL;
     text += "DATE " + clean(dateStr) + " " + clean(timeStr) + NL;
+    text += "------------------------------" + NL;
+
+    text += loteriesText;
     text += "------------------------------" + NL;
 
     const gameRows = Object.values(gameMap);
